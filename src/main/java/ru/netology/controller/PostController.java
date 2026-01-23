c10a582e7da4ccbe703884f112e5a8bd5a95b825
Andy Sh <andysh@Andys-MacBook-Pro.local>
2026-01-23 23:59:34 +0300

    feat: implement PostController getById and removeById

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -23,8 +23,11 @@ public class PostController {
     response.getWriter().print(gson.toJson(data));
   }
 
-  public void getById(long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
+  public void getById(long id, HttpServletResponse response) throws IOException {
+    response.setContentType(APPLICATION_JSON);
+    final var data = service.getById(id);
+    final var gson = new Gson();
+    response.getWriter().print(gson.toJson(data));
   }
 
   public void save(Reader body, HttpServletResponse response) throws IOException {
@@ -36,6 +39,6 @@ public class PostController {
   }
 
   public void removeById(long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
+    service.removeById(id);
   }
 }
