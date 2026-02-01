4e02f2270586bfc5285d77c363f74a868ab75450
Andy Sh <shinkarev89@gmail.com>
2026-02-01 22:26:41 +0300

    feat: add Spring annotation-based DI

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,9 +1,8 @@
 package ru.netology.servlet;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.netology.controller.PostController;
 import ru.netology.exception.NotFoundException;
-import ru.netology.repository.PostRepository;
-import ru.netology.service.PostService;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -20,9 +19,8 @@ public class MainServlet extends HttpServlet {
 
   @Override
   public void init() {
-    final var repository = new PostRepository();
-    final var service = new PostService(repository);
-    controller = new PostController(service);
+    final var context = new AnnotationConfigApplicationContext("ru.netology");
+    controller = context.getBean(PostController.class);
   }
 
   @Override
@@ -33,7 +31,7 @@ public class MainServlet extends HttpServlet {
       if (GET.equals(method) && path.equals(API_POSTS)) {
         controller.all(resp);
         return;
-      }
+       }
       if (GET.equals(method) && path.matches(API_POSTS_WITH_ID)) {
         final var id = extractId(path);
         controller.getById(id, resp);
